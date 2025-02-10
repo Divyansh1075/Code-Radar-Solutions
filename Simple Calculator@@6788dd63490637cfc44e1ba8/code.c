e163ceb6034450f801df4635c591f9d009a9ae44
Divyansh1075 <divyam1075@gmail.com>
2025-02-10 18:25:22 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-
 int main() {
-    int a,b;
+    int a, b;
     char symbol;
-    scanf("%i %i %c", &a, &b, &symbol);
-    if (symbol=='+'){
-        printf("%i", a+b);
-    }else if(symbol=='-'){
-        printf("%i", a-b);
-    }else if(symbol=='*'){
-        printf("%i", a*b);
-    }else if(symbol=='/'){
-        printf("%i", a/b);
-    }
-    return 0;
 
+    // Read input values for a, b, and the symbol
+    scanf("%i %i %c", &a, &b, &symbol);
+
+    // Perform the operation based on the symbol
+    if (symbol == '+') {
+        printf("%i", a + b);
+    } else if (symbol == '-') {
+        printf("%i", a - b);
+    } else if (symbol == '*') {
+        printf("%i", a * b);
+    } else if (symbol == '/') {
+        if (b != 0) {
+            printf("%i", a / b);
+        } else {
+            printf("Error: Division by zero");
+        }
+    } else if (symbol == '%') {
+        printf("%i", a % b);
+    } else {
+        printf("Error: Invalid symbol");
+    }
+
+    return 0;
 }
