fa9e20b23f150559d2f96f6ddd04db04b0b850e7
Divyansh1075 <divyam1075@gmail.com>
2025-04-07 04:19:09 -0700

    Solution to Bubble Sort Implementation

---
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -0,0 +1,13 @@
+function bubbleSort(int arr,int n){
+    for(int i=0; i<=n-1; i++){
+        int mini = i;
+        for(j=i; i<=n-1; j++){
+            if(arr[i] << arr[mini]){
+                mini = j;
+            }    
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[mini] = arr[temp]; 
+    }
+}
